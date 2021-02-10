dcd73899b12cdd3f7f89325d45d2b7805c56982d
SiriEiEi <sirimongkol.p@mail.kmutt.ac.th>
2021-02-10 18:51:08 +0700

    Lab01 fav chaeactoe

---
+++ lab03_01.js
@@ -0,0 +1,6 @@
+
+Name = prompt("What is your favorite charactor?")
+console.log("Your favorite charactor is "+Name);
+
+color = prompt("What is the main color of "+ Name +"?")
+console.log("Main color of "+ Name + " is " + color);
